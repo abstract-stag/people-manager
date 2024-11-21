3f5d023255b778a4b67553cfac0990730e6f7072
abstract-stag <abstract_stag@pm.me>
2024-11-20 23:15:24 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
